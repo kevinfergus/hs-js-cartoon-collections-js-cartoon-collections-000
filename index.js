d6658f417c5519d520b0f17e997d7d1014f5f594
Kevin Fergus <kevinfergus89@gmail.com>
2019-12-18 21:46:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function dwarfRollCall(dwarves) {
-  var string= []
-  let i=0
+  let rollCall = []
   for (let i=0; i<dwarves.length; i++) {
-    string.push(`${i+1}. ${dwarves[i]} `)
+    rollCall.push(`${i+1}. ${dwarves[i]} `)
   }
-  return string.join('')
+  return rollCall.join('')
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  let summon = []
+  
+  for (let i = 0; i <planeteerCalls.length; i ++) {
+    let call = `${planeteerCalls[i].toUpperCase()}!`
+    summon.push(call)
+   }
+   return summon
 }
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i < words.length; i ++) {
+    if (words[i].length>4) {
+      return true
+    }
+    return false
+  }
 }
 
 function findTheCheese (foods) {
+  for (let i = 0; i < foods.length; i ++) {
+    if (foods[i]==='cheddar' || foods[i]==='gouda' || foods[i] === 'camembert') {
+      return foods[i]
+    }
+  }
+  return 'no cheese!'
 }

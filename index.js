e3b66f5d637a27fb366fcd8da888f245186d2bce
Kevin Fergus <kevinfergus89@gmail.com>
2019-12-18 21:22:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function dwarfRollCall(dwarves) {
+  var string= []
+  let i=0
+  for (let i=0; i<dwarves.length; i++) {
+    string.push(`${i+1}. ${dwarves[i]} `)
+  }
+  return string.join('')
 }
 
 function summonCaptainPlanet(planeteerCalls){
